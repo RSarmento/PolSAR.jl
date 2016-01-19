e81cc5c9d4fe15c8466f7034c088a4c77f0fb4ab
RSarmento <rivo.sarmento@gmail.com>
2016-01-19 17:00:29 -0300

    Create PauliDecomposition.jl

---
+++ PauliDecomposition.jl
@@ -0,0 +1,15 @@
+function PauliDecomposition(mHH, mHV, mVV, height, widht)
+
+	pauliR = mHH + mVV
+
+	pauliG = abs(mVV - mHH)
+
+  pauliB = 2*mHV
+
+	pauliReq = ecdf(pauliR)(pauliR)
+	pauliGeq = ecdf(pauliG)(pauliG)
+	pauliBeq = ecdf(pauliB)(pauliB)
+	pauliRGBeq = reshape([[pauliReq],[pauliGeq],[pauliBeq]],(widht,height,3))
+
+	return pauliRGBeq
+end
